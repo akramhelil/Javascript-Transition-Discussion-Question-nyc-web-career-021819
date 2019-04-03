181053632cff96a1be48015c0d73d99b760a58dc
akramhelil <akramhelil@gmail.com>
2019-04-03 10:15:25 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,24 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+// $('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+//
+// function saveLincoln(interval) {
+//   if(isNaN(interval)) {
+//     interval = 10000
+//   }
+//   $('#foreground').fadeOut(interval);
+// }
 
-function saveLincoln(interval) {
-  if(isNaN(interval)) {
-    interval = 10000
-  }
-  $('#foreground').fadeOut(interval);
+const saveLincoln = document.getElementById("save_lincoln");
+const interval = parseInt(document.getElementbyId("interval")).value;
+const  foreGround = document.getElementbyId("foreground")
+let opacity = 1;
+
+
+function fadeOut() {
+  foreGround.style.opacity = opacity;
+  opacity -=  0.3;
 }
+
+
+saveLincoln.addEventListener("click", function(e){
+  console.log(e)
+})
